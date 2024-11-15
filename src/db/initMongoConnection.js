4173a298650477e8e8d75e0b072680531e7bdc50
Tetiana0310 <tetiana.posmashna@gmail.com>
2024-11-15 13:10:55 +0200

    Update initMongoConnection.js

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,5 +1,4 @@
 import mongoose from 'mongoose';
-
 import { env } from "../utils/env.js";
 
 export const initMongoConnection = async () => {
@@ -9,9 +8,9 @@ export const initMongoConnection = async () => {
     const url = env('MONGODB_URL');
     const db = env('MONGODB_DB');
 
-    await mongoose.connect(
-      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`,
-    );
+    const connectionString = `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`;
+
+    await mongoose.connect(connectionString);
     console.log('Mongo connection successfully established!');
   } catch (e) {
     console.log('Error while setting up mongo connection', e);
