1c2003db246f24b26f47a8516587a58120c24a5a
Tetiana0310 <tetiana.posmashna@gmail.com>
2024-12-04 10:18:20 +0200

    validation

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -7,14 +7,28 @@ import {
   postContactController,
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import {
+  createContactSchema,
+  updateContactSchema,
+} from '../validation/contacts.js';
+import { isValidId } from '../middlewares/isValidId.js';
 
 const router = Router();
 
 router.get('/', ctrlWrapper(getContactsController));
 router.get('/:contactId', ctrlWrapper(getContactController));
-router.post('/', ctrlWrapper(postContactController));
-router.patch('/:contactId', ctrlWrapper(patchContactController));
+router.post(
+  '/',
+  validateBody(createContactSchema),
+  ctrlWrapper(postContactController),
+);
+router.patch(
+  '/:contactId',
+  isValidId,
+  validateBody(updateContactSchema),
+  ctrlWrapper(patchContactController),
+);
 router.delete('/:contactId', ctrlWrapper(deleteContactController));
 
 export default router;
-
