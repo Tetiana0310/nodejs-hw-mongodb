9d2dcc86aa30acbab011a1c54e79dfc1741b93ad
Tetiana0310 <tetiana.posmashna@gmail.com>
2024-12-22 22:18:11 +0200

    Update package.json

--- src/server.js
+++ src/server.js
@@ -27,6 +27,7 @@ export const setupServer = () => {
     }),
   );
 
+  app.use('/api-docs', ...swaggerDocs());
   app.use(router);
 
   app.use('*', notFoundHandler);
@@ -35,6 +36,4 @@ export const setupServer = () => {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
-  app.use('/uploads', express.static(UPLOAD_DIR));
-  app.use('/api-docs', swaggerDocs());
 };
