66909b8d1c6eb615a212cf251ec79e84567f7bff
Tetiana0310 <tetiana.posmashna@gmail.com>
2024-12-22 18:11:03 +0200

    google auth

--- src/services/auth.js
+++ src/services/auth.js
@@ -10,6 +10,7 @@ import handlebars from 'handlebars';
 import path from 'node:path';
 import fs from 'node:fs/promises';
 import { sendEmail } from '../utils/sendMail.js';
+import { getFullNameFromGoogleTokenPayload, validateCode } from '../utils/googleOAuth2.js';
 
 export const findUser = (filter) => UsersCollection.findOne(filter);
 
@@ -174,4 +175,30 @@ export const resetPassword = async (payload) => {
   );
 
   return { message: 'Password reset successful' };
+};
+
+// Сьоме домащне завдання гугл
+
+export const loginOrSignupWithGoogle = async (code) => {
+  const loginTicket = await validateCode(code);
+  const payload = loginTicket.getPayload();
+  if (!payload) throw createHttpError(401);
+
+  let user = await UsersCollection.findOne({ email: payload.email });
+  if (!user) {
+    const password = await bcrypt.hash(randomBytes(10), 10);
+    user = await UsersCollection.create({
+      email: payload.email,
+      name: getFullNameFromGoogleTokenPayload(payload),
+      password,
+      role: 'parent',
+    });
+  }
+
+  const newSession = createSession();
+
+  return await SessionsCollection.create({
+    userId: user._id,
+    ...newSession,
+  });
 };
